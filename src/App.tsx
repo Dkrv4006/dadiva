6ad1246a5617f41345785ce4bc80e4003bd492f1
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-11-24 23:42:37 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Button } from "@mui/material";
+import { Home } from "./components/style";
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+        <Button variant="contained">
+          daniel
+        </Button>
+         <Home>
+          daniel
+         </Home>
         <h1>daniel</h1>
       </header>
     </div>
