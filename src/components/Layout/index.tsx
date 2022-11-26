1f51d50f09e2932c81bd3f96f3d1cc3483ae2c3d
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-11-26 13:20:38 -0300

    Create index.tsx

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,11 @@
+import TypeIt from "typeit-react";
+import { LayoutStyle } from "./style";
+
+export function Layout(){
+    return(
+       <LayoutStyle>
+           <TypeIt>This will be typed in a `span` element!</TypeIt>
+    
+       </LayoutStyle>
+    )
+}
