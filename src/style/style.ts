b9e57954939b7e1a44903b9dea6bdcda898d5702
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-11-27 04:07:46 -0300

    Update style.ts

--- src/style/style.ts
+++ src/style/style.ts
@@ -5,10 +5,22 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
      }
      body, html{
       background: #353535;
       /* background-image: linear-gradient(to left top, #666869, #4e4f50, #363737, #202021, #070707); */
+     }
+
+     li{
+      list-style: none;
+      float: left;
+      margin: 2.5rem 20px;
+
+     }
+     a{
+      text-decoration: none;
+      color: #0DA1FF;
      }
 
      .input-g{
@@ -19,7 +31,7 @@ export const GlobalStyle = createGlobalStyle`
         transition: 150ms cubic-bezier(0.4,0,0.2,1);
      }
      
-     label, h1{
+     label{
         position: absolute;
         left: 10px;
         background: white;
@@ -32,7 +44,7 @@ export const GlobalStyle = createGlobalStyle`
         border: 1.5px solid red;
      }
 
-     input:hover ~ h1{
+     input:hover ~ label{
         transform: translateY(-50%) scale(0.8)
         
      }
