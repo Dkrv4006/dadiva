f63031c5d8b3f3c6d1f5a4b0c949e864eef7e148
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-11-27 04:08:05 -0300

    Update style.ts

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -6,5 +6,25 @@ export const Header = styled.div`
     width: 100%;
     height: 100px;
     max-width: 1400px;
-    background: red;
-`
+    
+    display: flex;
+    justify-content: space-around;
+    
+    span{
+        color: white;
+        font-weight: 700;
+    }
+    .logo{
+        margin-top: 2.5rem;
+        color: #0DA1FF;
+        font-weight: 400;
+        
+    }
+
+    @media screen and (max-width: 680px) {
+        .navegation, .social{
+            display: none;
+        }
+
+    }
+`;
