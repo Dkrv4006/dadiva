4069b0336b1a1a331ff7aa78c57e51f73bf5e68f
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-11-27 04:07:49 -0300

    Create style.ts

---
+++ src/components/MainInfor/style.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+
+export const MainStyle = styled.div`
+    width: 100%;
+    height: 500px;
+    
+
+    display: flex;
+    
+    .divleft{
+        width: 50%;
+       
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .divright{
+        width: 50%;
+        background: blue;
+    }
+
+    span{
+        color: #0DA1FF;
+    }
+    h1{
+        color: white;
+    }
+
+    @media screen and (max-width: 680px) {
+        .divright{
+            display: none;
+        }
+
+        .divleft{
+            width: 100%;
+            height: 100px;
+        }
+        h1{
+            font-size: 20px;
+            padding: 20px;
+        }
+
+    }
+
+`
