41895c598d072a36479695d75271fe3de49ac341
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-11-27 04:07:52 -0300

    Create index.tsx

---
+++ src/components/MainInfor/index.tsx
@@ -0,0 +1,13 @@
+import { MainStyle } from "./style";
+
+export function MainInfor(){
+    return(
+
+        <MainStyle>
+            <div className="divleft">
+                <h1>Olá meu nome e Daniel <br /> sou desenvouvedor <span>FULL STACK</span></h1>
+            </div>
+            <div className="divright">b</div>
+        </MainStyle>
+    )
+}
