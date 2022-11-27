73786cd902d43f87d36ebd573913fcb0d417f8a8
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-11-27 04:08:08 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,23 @@ import { Header } from "./style"
 export function Heade(){
     return(
         <Header>
-        
+            <div className="logo"><span>DANIEL </span>dev</div>
+            <div className="navegation">
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contate</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="social">
+            <ul>
+                <li><a href="#">GitHunb</a></li>
+                <li><a href="#">Linkdin</a></li>
+                <li><a href="#">CodePen</a></li>
+            </ul>
+            </div>
         </Header>
     )
 }
