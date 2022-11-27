b7d07ef9b4061e997e01eabe262da6705c0a2db9
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-11-27 04:07:56 -0300

    Create index.tsx

---
+++ src/components/leyaut/index.tsx
@@ -0,0 +1,18 @@
+
+import { Heade } from "../Header";
+import { MainInfor } from "../MainInfor";
+import { LayoutStyle } from "./style";
+
+export function Layout(){
+   
+
+    return(
+      <LayoutStyle>
+        <Heade/>
+        <MainInfor/>
+      </LayoutStyle>
+    
+    )
+
+    
+}
