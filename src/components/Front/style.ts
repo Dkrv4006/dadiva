455655593d5d40485438e127d7a90375338f5952
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-11-29 20:53:55 -0300

    Update style.ts

--- src/components/Front/style.ts
+++ src/components/Front/style.ts
@@ -10,6 +10,24 @@ export const FrontStyle = styled.div`
         align-items: center;
         gap: 10px;
         flex-wrap: wrap;
+        position: relative;
+    @media screen and (max-width: 441px) {
+        &:after{
+        content: "";
+        position: absolute;
+        width: 153px;
+        height:500px;
+        border-bottom:rgba(333,333,333,0.3) solid 3px;
+        border-left: rgba(333,333,333,0.3) solid 3px;
+        border-top: transparent solid 8px;
+        border-right: transparent solid 8px;
+        border-radius: 10px;
+        bottom: -40px;
+        left: 9px;
+        z-index: 0;
+        
+        }
+    }
     }
     
 
@@ -17,8 +35,9 @@ export const FrontStyle = styled.div`
         width: 200px;
         height: 80px;
        
-        
-        background: gray;
+        box-shadow:0px 0px 5px #0DA1FF;
+        background: #4e4f50 ;
+       
     }
  
 
