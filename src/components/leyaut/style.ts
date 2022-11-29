bb56105743a305795d485433e3b41c18b75a07e1
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-11-29 04:03:44 -0300

    Update style.ts

--- src/components/leyaut/style.ts
+++ src/components/leyaut/style.ts
@@ -6,7 +6,7 @@ export const LayoutStyle = styled.div`
     height: 1000px;
     margin:20px 0;
     background: #353535;
-    box-shadow:0px 0px 19px rgba(0, 0, 0, 0.64);
+    /* box-shadow:0px 0px 19px #0DA1FF; */
     border-radius: 20px;
 
   /* background-image: linear-gradient(to right bottom, #666869, #4e4f50, #363737, #202021, #070707); */
