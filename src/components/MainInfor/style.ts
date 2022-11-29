17b59f8f111a70dfb4ab45d36a0c4dadee0f6bf0
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-11-29 20:53:53 -0300

    Update style.ts

--- src/components/MainInfor/style.ts
+++ src/components/MainInfor/style.ts
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import reactjs from '../../asses/reactjs.svg'
+import nodejs from '../../asses/nodejs.svg'
+import javascript from '../../asses/javascript.svg'
 
 
 export const MainStyle = styled.div`
@@ -18,9 +21,51 @@ export const MainStyle = styled.div`
         align-items: center;
 
         border-bottom: #0DA1FF solid 2px;
+
+        position: relative;
+
+        &::before{
+        content: url(${javascript});
+
+        position: absolute;
+        height: 80px;
+        width: 80px;
+        bottom: -15%;
+        left: 50%;
+        color: red;
+        transform: translate(-50%, -50%);
+        }
     }
     .divright{
         width: 50%;
+        border-bottom: #0DA1FF solid 2px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+
+        &::after{
+        content: url(${reactjs});
+      
+        position: absolute;
+        height: 100px;
+        width: 100px;
+        top: 120%;
+        left: 80%;
+        transform: translate(-50%, -50%);
+        }
+
+        &::before{
+        content: url(${nodejs});
+
+        position: absolute;
+        height: 100px;
+        width: 100px;
+        top: 10%;
+        left: 10%;
+        color: red;
+        transform: translate(-50%, -50%);
+        }
         
     }
 
@@ -39,12 +84,20 @@ export const MainStyle = styled.div`
         .divleft{
             width: 80%;
             height: 100px;
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
         }
         h1{
             font-size: 20px;
             padding: 20px;
         }
 
+    }
+    img{
+        width: 400px;
+        height: 400px;
     }
 
 `
