71d42cf1499127798dc5a4643b7cae6e1481d8b3
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-11-29 04:03:53 -0300

    Create index.tsx

---
+++ src/components/Front/index.tsx
@@ -0,0 +1,16 @@
+import { FrontStyle } from "./style";
+
+
+
+export function Front(){
+    return(
+        <FrontStyle>
+            <div className="front">
+                <div className="js">1</div>
+                <div className="js">2</div>
+                <div className="js">3</div>
+                <div className="js">4</div>
+            </div>
+        </FrontStyle>
+    )
+}
