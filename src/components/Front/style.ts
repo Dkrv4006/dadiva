f869e5692c05bef1b85423d4c3cdeeeeced043cd
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-11-29 04:03:49 -0300

    Create style.ts

---
+++ src/components/Front/style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const FrontStyle = styled.div`
+    width: 100%;
+    height: 200px;
+
+    .front{
+        display: flex;
+        justify-content:center;
+        align-items: center;
+        gap: 10px;
+        flex-wrap: wrap;
+    }
+    
+
+    .js{
+        width: 200px;
+        height: 80px;
+       
+        
+        background: gray;
+    }
+ 
+
+  
+`
