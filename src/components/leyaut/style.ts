eab539bf54086760ca4bd93ecd44d4b3ad2f252d
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-12-03 05:15:07 -0300

    Update style.ts

--- src/components/leyaut/style.ts
+++ src/components/leyaut/style.ts
@@ -4,23 +4,23 @@ export const LayoutStyle = styled.div`
     width: auto;
     height: auto;
 
-    background-color: azure;
+    background-color:#191923;
 
     main{
         
-        padding: 15px;
+        padding: 15px 15px 15px 15px;
         margin: 0;
         display: flex;
         justify-content: center;
 
+        
     }
     
     .app-root{
-       
         max-width: 1400px;
 
         display: grid;
-        grid-template-columns: 1fr 4fr 0.5fr;
+        grid-template-columns: 1fr 4fr 0.1fr;
         gap: 20px;
         
     }
