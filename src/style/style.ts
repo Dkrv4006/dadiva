f716de1a5f791337284ca55444ac2a0415ab4b2e
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-12-03 05:15:22 -0300

    Update style.ts

--- src/style/style.ts
+++ src/style/style.ts
@@ -12,6 +12,14 @@ export const GlobalStyle = createGlobalStyle`
       overflow: visible;
       /* background-image: linear-gradient(to left top, #666869, #4e4f50, #363737, #202021, #070707); */
      }
+     body::-webkit-scrollbar{
+        width: 5px;
+        background-color: #353535;
+    }
+     body::-webkit-scrollbar-thumb{
+        width: 5px;
+        background-color: #0016FF;
+    }
 
      #root{
     
