3fa5ea0c0fd742cab6f1f102820c8c80553ef00a
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-12-05 19:12:10 -0300

    Update index.tsx

--- src/components/MainCenter/liga/index.tsx
+++ src/components/MainCenter/liga/index.tsx
@@ -1,11 +1,14 @@
 import { LigaStyle } from "./style"
-// import "@hover.css"
+
 
 
 export const Liga = () => {
   return (
     <LigaStyle>
-        <div className="frontend hvr-grow "><h1> FrontEnd</h1></div>
+        <div className="frontend hvr-grow ">
+          <h1> FrontEnd</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et leo ac libero vulputate ultrices. Nullam tristique orci sit amet lacus tincidunt, a hendrerit tellus ullamcorper. Aenean feugiat metus in nunc tincidunt faucibus. Duis nec venenatis metus. Duis rhoncus et sapien quis consequat.</p>
+          </div>
         <div className="backend hvr-grow"><h1>BackEnd</h1></div>
         <div className="ciby hvr-grow"><h1>Ciby</h1></div>
         
