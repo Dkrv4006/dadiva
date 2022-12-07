9dcb7a25bcc0964343be25850c6f1ad52f49cae6
Daniel_Kennedy <92266495+Dkrv4006@users.noreply.github.com>
2022-12-07 01:22:40 -0300

    Update index.tsx

--- src/components/MainCenter/project/index.tsx
+++ src/components/MainCenter/project/index.tsx
@@ -7,8 +7,8 @@ export const Project = () => {
        <h1>Projetos</h1>
        <div className="projectMain">
        <div className="project"><i className="fa-solid fa-spinner fa-spin-pulse"></i></div>
-       <div className="project">2</div>
-       <div className="project">3</div>
+       <div className="project"><i className="fa-solid fa-spinner fa-spin-pulse"></i></div>
+       <div className="project"><i className="fa-solid fa-spinner fa-spin-pulse"></i></div>
 
        </div>
     </ProjectStyle>
